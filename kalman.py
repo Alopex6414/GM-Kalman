2ed5d6aa9d08838ac40b0730c6e83da4f7235bd8
Alopex6414 <1029535012@qq.com>
2022-05-20 23:13:32 +0800

    Create class Schedule.

--- kalman.py
+++ kalman.py
@@ -3,10 +3,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+class Schedule(object):
+    def __init__(self, period):
+        """
+        :param period: please input schedule period
+        """
+        self.period = period
+        self.sigma = 0.01
+        self.mu = 1 / period
+        self.velocity = self.sigma * np.random.randn(self.period) + self.mu
+        self.progress = np.cumsum(self.velocity)
+
+
 if __name__ == '__main__':
-    arr1 = np.random.random(10)
-    print("arr1:{}", arr1)
+    s = Schedule(15)
+    print("velocity:{}", s.velocity)
     plt.figure()
-    plt.plot(arr1)
+    plt.plot(s.velocity, 'bo')
+    plt.plot(s.progress, 'rx--')
     plt.show()
     pass
