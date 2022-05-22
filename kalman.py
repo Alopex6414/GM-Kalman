317fc1632dca2bc006043811bd053f5062c27437
Alopex6414 <1029535012@qq.com>
2022-05-22 16:25:29 +0800

    Add Kalman class to filter original progress & velocity data.

--- kalman.py
+++ kalman.py
@@ -39,15 +39,50 @@ class Schedule(object):
                 progress = 1.0
             self.velocity = np.append(self.velocity, velocity)
             self.progress = np.append(self.progress, progress)
+        self.velocity = np.append(self.velocity, 0.0)
+
+
+class KalmanFilter(object):
+    def __init__(self, progress, velocity):
+        """
+        :param progress: original project progress status
+        :param velocity: original project velocity status
+        """
+        self.len = len(progress)
+        self.Z = np.zeros((2, self.len))
+        self.Z[0, :] = progress
+        self.Z[1, :] = velocity
+        self.K = np.zeros((1, self.len))
+        self.X = np.zeros((2, self.len))
+        self.P = np.array([[1., 0.], [0., 1.]])
+        self.F = np.array([[1., 1.], [0., 1.]])
+        self.U = 0.01
+        self.V = 0.01
+        self.Q = np.array([[self.U, 0.], [0., self.U]])
+        self.R = np.array([[self.V, 0.], [0., self.V]])
+        self.E = np.eye(2)
+
+    def filter(self):
+        for i in range(2, self.len):
+            self.X[:, i] = np.dot(self.F, self.X[:, i - 1])
+            self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+            self.K = self.P / (self.P + self.R)
+            self.X[:, i] = self.X[:, i - 1] + np.dot(self.K, (self.Z[:, i] - self.X[:, i]))
+            self.X[0, i] = self.X[0, i - 1] + self.X[1, i - 1]
+            self.P = np.dot((self.E - self.K), self.P)
 
 
 if __name__ == '__main__':
     s = Schedule(15)
     s.gen()
+    kf = KalmanFilter(s.progress, s.velocity)
+    kf.filter()
     print("progress:", s.progress)
     print("velocity:", s.velocity)
     plt.figure()
     plt.plot(s.progress, 'rx--')
     plt.plot(s.velocity, 'bo')
+    plt.plot(kf.X[0, :], 'gx--')
+    plt.plot(kf.X[1, :], 'yo')
     plt.show()
     pass
