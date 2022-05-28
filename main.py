6c43ef73609e521583e3318abba13179f27de6fe
Alopex6414 <1029535012@qq.com>
2022-05-28 21:26:17 +0800

    Add plot title & axis label & grid.

--- main.py
+++ main.py
@@ -26,15 +26,24 @@ if __name__ == '__main__':
     print("velocity:", s.velocity)
     plt.figure()
     plt.plot(s.progress, 'rx--')
-    plt.plot(s.velocity, 'bo')
+    plt.plot(s.velocity, 'bo:')
     plt.plot(kf.X[0, :], 'gx--')
-    plt.plot(kf.X[1, :], 'yo')
+    plt.plot(kf.X[1, :], 'yo:')
     # plt.plot(gm.G, 'mx--')
     # test
     for i in range(5, 14):
         gm = GM(kf.X, i)
         gm.gray_predict()
         plt.plot(gm.G, 'mx--')
+
+    # plot title & axis label
+    plt.xlabel("time")
+    plt.ylabel("progress")
+    plt.title("GM-Kalman Simulation")
+
+    # plot grid
+    plt.grid(linestyle='--', linewidth=1.0)
+
     plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
