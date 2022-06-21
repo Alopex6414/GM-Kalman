48bd1f92c368672b4ed5bc84e5a1d021f02f68b3
Alopex6414 <1029535012@qq.com>
2022-06-21 21:15:42 +0800

    Add Static Partition Control Class to analysis activities status.

--- static.py
+++ static.py
@@ -19,6 +19,7 @@ class StaticPartition(object):
         self.period = t
         self.actual = n
         self.real = array[0, n] * t
+        self.delta = self.actual - self.real
         self.green = self.buffer * 1. / 3
         self.yellow = self.buffer * 2. / 3
         self.red = self.buffer
@@ -27,12 +28,38 @@ class StaticPartition(object):
     def static_analysis(self):
         buf_size = self.actual - self.real
         if buf_size < self.green:
-            self.status.update({"{}".format(self.actual): "G"})
+            result = 0
         elif buf_size < self.yellow:
-            self.status.update({"{}".format(self.actual): "Y"})
+            result = 1
         else:
-            self.status.update({"{}".format(self.actual): "R"})
-        print("hello")
+            result = 2
+        return result
+
+
+class StaticPartitionControl(StaticPartition):
+    Status = dict()
+
+    def __init__(self, array, buffer, n, t):
+        """
+        :param array: project schedule progress status array (numpy array type) (2x2)
+        :param buffer: project schedule progress feeding or project buffer
+        :param n: project schedule progress status array actual length
+        :param t: project schedule progress periodic
+        """
+        super(StaticPartitionControl, self).__init__(array, buffer, n, t)
+
+    def static_analysis(self):
+        result = super(StaticPartitionControl, self).static_analysis()
+        if result == 0:
+            StaticPartitionControl.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                                             "delta": self.delta, "status": "G"}})
+        elif result == 1:
+            StaticPartitionControl.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                                             "delta": self.delta, "status": "Y"}})
+        else:
+            StaticPartitionControl.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                                             "delta": self.delta, "status": "R"}})
+        print(result)
 
 
 if __name__ == '__main__':
@@ -43,6 +70,7 @@ if __name__ == '__main__':
     # kalman filter
     kalman = KalmanFilter(schedule.progress, schedule.velocity)
     kalman.filter()
-    for i in range(period):
-        s = StaticPartition(kalman.X, 5, i, period)
+    for i in range(len(kalman.X[0])):
+        s = StaticPartitionControl(kalman.X, 5, i, period)
         s.static_analysis()
+    print("hello")
