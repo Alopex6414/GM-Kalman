54f863a54f85d9863abe002da792f54b8d936271
Alopex6414 <1029535012@qq.com>
2022-06-26 15:56:32 +0800

    Add Relative Partition Class.

---
+++ relative.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+
+from schedule import Schedule
+from kalman import KalmanFilter
+
+
+class RelativePartition(object):
+    def __init__(self, array, buffer, n, t):
+        """
+        :param array: project schedule progress status array (numpy array type) (2x2)
+        :param buffer: project schedule progress feeding or project buffer
+        :param n: project schedule progress status array actual length
+        :param t: project schedule progress periodic
+        """
+        self.array = array
+        self.buffer = buffer
+        self.period = t
+        self.actual = n
+        self.real = array[0, n] * t
+        self.delta = self.actual - self.real
+        self.green = self.buffer * 1. / 3
+        self.yellow = self.buffer * 2. / 3
+        self.red = self.buffer
+        self.status = dict()
+
+
+if __name__ == '__main__':
+    pass
