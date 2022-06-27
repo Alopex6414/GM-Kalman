79c540d36f4d87e7bedc488b01446d46b0cbc03f
Alopex6414 <1029535012@qq.com>
2022-06-27 21:03:56 +0800

    Add Plot for Relative.

--- relative.py
+++ relative.py
@@ -87,4 +87,71 @@ class RelativePartitionControl(RelativePartition):
 
 
 if __name__ == '__main__':
-    pass
+    period = 15
+    # generate schedule
+    schedule = Schedule(period)
+    schedule.gen()
+    # kalman filter
+    kalman = KalmanFilter(schedule.progress, schedule.velocity)
+    kalman.filter()
+    # setup array
+    RelativePartitionControl.setup_array(kalman.X)
+    for i in range(len(kalman.X[0])):
+        s = RelativePartitionControl(kalman.X, 5, i, period)
+        s.relative_analysis()
+        s.relative_control()
+    # static safe buffer progress
+    green = np.zeros(len(kalman.X[0]))
+    yellow = np.zeros(len(kalman.X[0]))
+    red = np.zeros(len(kalman.X[0]))
+    for i in range(len(kalman.X[0])):
+        # green buffer
+        green[i] = i * 1. / (s.period + s.green)
+        if green[i] > 1.:
+            green[i] = 1.
+        # yellow buffer
+        yellow[i] = i * 1. / (s.period + s.yellow)
+        if yellow[i] > 1.:
+            yellow[i] = 1.
+        # red buffer
+        red[i] = i * 1. / (s.period + s.red)
+        if red[i] > 1.:
+            red[i] = 1.
+    # static safe buffer consume
+    buf_G = np.zeros(len(kalman.X[0]))
+    buf_Y = np.zeros(len(kalman.X[0]))
+    buf_R = np.zeros(len(kalman.X[0]))
+    status = np.zeros(len(kalman.X[0]))
+    for i in range(len(kalman.X[0])):
+        buf_G[i] = s.green
+        buf_Y[i] = s.yellow
+        buf_R[i] = s.red
+        status[i] = s.Status.get("{}".format(i))["delta"]
+    # subplot1 line (Progress)
+    x = np.arange(len(s.array[0]))
+    plt.figure()
+    plt.plot(x, s.array[0], color="lightskyblue", marker="o", linestyle="--", label="Actual Progress")
+    plt.plot(x, s.X[0], color="red", marker="o", linestyle="--", label="Control Progress")
+    plt.plot(x, green, color="lightgreen", marker="o", linestyle="--", label="Green Progress")
+    plt.plot(x, yellow, color="orange", marker="o", linestyle="--", label="Yellow Progress")
+    plt.plot(x, red, color="lightcoral", marker="o", linestyle="--", label="Red Progress")
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel("Time")
+    plt.ylabel("Progress")
+    plt.title("Project Progress Status")
+    # plt.savefig("./figure/deviation.png")
+    plt.show()
+    # subplot1 line (Buffer Consume)
+    plt.figure()
+    plt.plot(x, status, color="lightskyblue", marker="o", linestyle="--", label="Actual Buffer Consume")
+    plt.plot(x, buf_G, color="lightgreen", marker="o", linestyle="--", label="Green Buffer")
+    plt.plot(x, buf_Y, color="orange", marker="o", linestyle="--", label="Yellow Buffer")
+    plt.plot(x, buf_R, color="lightcoral", marker="o", linestyle="--", label="Red Buffer")
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel("Time")
+    plt.ylabel("Buffer")
+    plt.title("Project Buffer Consume")
+    # plt.savefig("./figure/deviation.png")
+    plt.show()
