de4767669c445c2fbbf9fc3d917ca9509f755197
Alopex6414 <1029535012@qq.com>
2022-07-02 10:54:55 +0800

    Add 3 monitor methods.

--- experiment.py
+++ experiment.py
@@ -34,11 +34,8 @@ class ExperimentSingle(object):
         self.kalman = None
         self.gm = None
         self.gmc = None
-        self.sp = None
         self.spc = None
-        self.rp = None
         self.rpc = None
-        self.dp = None
         self.dpc = None
 
     def simulate(self):
@@ -48,14 +45,23 @@ class ExperimentSingle(object):
         # kalman filter
         self.kalman = KalmanFilter(self.schedule.progress, self.schedule.velocity)
         self.kalman.filter()
-        # GM baseline
+        # GM/SP/RP/DP baseline
         GMControl.setup_array(self.kalman.X)
-        # GM predict
+        SPControl.setup_array(self.kalman.X)
+        RPControl.setup_array(self.kalman.X)
+        DPControl.setup_array(self.kalman.X)
+        # GM/SP/RP/DP predict
         for i in range(5, len(self.kalman.X[0])):
             self.gm = GM(self.kalman.X, self.buffer, i)
             self.gm.gray_predict2()
             self.gmc = GMControl(self.kalman.X, self.buffer, i, self.period)
             self.gmc.gray_predict2()
+            self.spc = SPControl(self.kalman.X, self.buffer, i, self.period)
+            self.spc.static_control()
+            self.rpc = RPControl(self.kalman.X, self.buffer, i, self.period)
+            self.rpc.relative_control()
+            self.dpc = DPControl(self.kalman.X, self.buffer, i, self.period)
+            self.dpc.dynamic_control()
         # calculate project finish time...
         # critical chain finish time
         b = False
@@ -75,6 +81,33 @@ class ExperimentSingle(object):
                 break
         if not b:
             self.time_gm = len(GMControl.X[0, :])
+        # static partition finish time
+        b = False
+        for i in range(0, len(SPControl.X[0, :])):
+            if SPControl.X[0, i] >= 1.:
+                b = True
+                self.time_sp = i
+                break
+        if not b:
+            self.time_sp = len(SPControl.X[0, :])
+        # relative partition finish time
+        b = False
+        for i in range(0, len(RPControl.X[0, :])):
+            if RPControl.X[0, i] >= 1.:
+                b = True
+                self.time_rp = i
+                break
+        if not b:
+            self.time_rp = len(RPControl.X[0, :])
+        # dynamic partition finish time
+        b = False
+        for i in range(0, len(DPControl.X[0, :])):
+            if DPControl.X[0, i] >= 1.:
+                b = True
+                self.time_dp = i
+                break
+        if not b:
+            self.time_dp = len(DPControl.X[0, :])
 
 
 if __name__ == '__main__':
