de54fcf5bb8e8d06fd7531523c5d527fc3175b1e
Alopex6414 <1029535012@qq.com>
2022-07-17 14:03:12 +0800

    Add SBMAC class.

--- sbma.py
+++ sbma.py
@@ -7,6 +7,7 @@ import copy
 from schedule import Schedule
 from kalman import KalmanFilter
 
+
 class SBMA(object):
     def __init__(self, array, buffer, n, t):
         self.array = array
@@ -30,6 +31,57 @@ class SBMA(object):
             # dangerous area
             result = 2
         return result
+
+
+class SBMAC(SBMA):
+    X = None
+    Count = 0
+    Status = dict()
+    
+    def __init__(self, array, buffer, n, t):
+        super(SBMAC, self).__init__(array, buffer, n, t)
+
+    @staticmethod
+    def setup_array(array):
+        SBMAC.X = copy.deepcopy(array)
+        SBMAC.Count = 0
+        SBMAC.Status = dict()
+
+    def sbma_analysis(self):
+        result = super(SBMAC, self).sbma_analysis()
+        if result == 0:
+            SBMAC.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                            "delta": self.delta, "status": "G", "risk": "low",
+                                                            "control": 0}})
+        elif result == 1:
+            SBMAC.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                            "delta": self.delta, "status": "Y", "risk": "medium",
+                                                            "control": 1}})
+        else:
+            SBMAC.Status.update({"{}".format(self.actual): {"real": self.real, "actual": self.actual,
+                                                            "delta": self.delta, "status": "R", "risk": "high",
+                                                            "control": 1}})
+        return result
+
+    def sbma_control(self):
+        result = self.sbma_analysis()
+        # project buffer consume result
+        if result == 0:
+            SBMAC.X[1, self.actual] = SBMAC.X[1, self.actual]
+        elif result == 1:
+            SBMAC.X[1, self.actual] = SBMAC.X[1, self.actual] + \
+                                                       0.025 * (1. - self.array[0, self.actual])
+            SBMAC.Count = SBMAC.Count + 1
+        else:
+            SBMAC.X[1, self.actual] = SBMAC.X[1, self.actual] + \
+                                                       0.05 * (1. - self.array[0, self.actual])
+            SBMAC.Count = SBMAC.Count + 1
+        # update current progress
+        if self.actual > 0:
+            SBMAC.X[0, self.actual] = SBMAC.X[0, self.actual - 1] + \
+                                                       SBMAC.X[1, self.actual]
+        if SBMAC.X[0, self.actual] > 1.:
+            SBMAC.X[0, self.actual] = 1.
 
 
 if __name__ == '__main__':
