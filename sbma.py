7ac5cf4b43b47d545f5c5d27140f31b5225d83ad
Alopex6414 <1029535012@qq.com>
2022-07-22 17:42:51 +0800

    Add SBMA analysis function.

--- sbma.py
+++ sbma.py
@@ -14,8 +14,7 @@ class SBMA(object):
         self.buffer = buffer
         self.period = t
         self.actual = n
-        self.real = array[0, n] * t
-        self.delta = self.actual - self.real
+        self.current = array[0, :n]
         self.SLT = np.zeros(10)
         self.SUT = np.zeros(10)
         self.simcount = 100
@@ -45,7 +44,28 @@ class SBMA(object):
             cost_sort = np.sort(cost)
             self.SLT[j] = cost_sort[int(self.simcount * self.alpha)]
             self.SUT[j] = cost_sort[int(self.simcount * (1. - self.alpha))]
-        pass
+    
+    def sbma_analysis(self):
+        """analysis buffer cost"""
+        # analysis buffer cost
+        time = np.arange(len(self.current))
+        rate = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        cost = np.interp(rate, self.current, time)
+        n = 0
+        for i in range(0, len(cost)):
+            if cost[i] == self.actual - 1:
+                n = i
+                break
+        cost = cost[:n]
+        # evaluate buffer cost
+        n = len(cost) - 1
+        if cost[-1] < self.SLT[n]:
+            result = 0
+        elif cost[-1] < self.SUT[n]:
+            result = 1
+        else:
+            result = 2
+        return result
 
 
 class SBMAC(SBMA):
@@ -62,5 +82,6 @@ if __name__ == '__main__':
     kalman = KalmanFilter(schedule.progress, schedule.velocity)
     kalman.filter()
     # setup SBMA
-    sbma = SBMA(kalman.X, 5, 0, period)
+    sbma = SBMA(kalman.X, 5, 12, period)
     sbma.sbma_simulate()
+    sbma.sbma_analysis()
