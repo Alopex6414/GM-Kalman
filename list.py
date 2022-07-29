b701176ac3310e6fcf760016b44389e9fe2d84c2
Alopex6414 <1029535012@qq.com>
2022-07-29 19:52:56 +0800

    Add more function for list class

--- list.py
+++ list.py
@@ -28,7 +28,54 @@ class Linklist(object):
             count += 1
             cur = cur.next
         return count
-
+    
+    def append(self, item):
+        node = Node(item)
+        if self.is_empty():
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+    
+    def add(self, item):
+        node = Node(item)
+        node.next = self.head
+        self.head = node
+    
+    def insert(self, pos, item):
+        if pos <= 0:
+            self.add(item)
+        elif pos > self.length() - 1:
+            self.append(item)
+        else:
+            pre = self.head
+            count = 0
+            while count < pos - 1:
+                count += 1
+                pre = pre.next
+            node = Node(item)
+            node.next = pre.next
+            pre.next = node
+    
+    def search(self, item):
+        cur = self.head
+        while cur == None:
+            if cur.value == item:
+                return True
+            else:
+                cur = cur.next
+        return False
+    
+    def remove(self, item):
+        cur = self.head
+        if cur.value == item:
+            self.head = cur.next
+        else:
+            while cur.next.value != item:
+                cur = cur.next
+            cur.next = cur.next.next
 
 
 if __name__ == '__main__':
