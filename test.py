64781c7e5d03e5f6b152a93f44b5ec0914932e6b
Alopex6414 <1029535012@qq.com>
2022-08-19 20:54:44 +0800

    Add test.py to test data.

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    data = np.random.lognormal(1, 1, 10000)
+    print(data)
+    pass
